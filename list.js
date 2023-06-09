46e02b12c7d1571b0c17fc53e13870af2c215167
albegosu <albegosu@gmail.com>
2023-06-09 15:28:24 +0200

    Add, edit, delete

--- list.js
+++ list.js
@@ -29,3 +29,106 @@ window.addEventListener('load', () => {
         showNotes();
     })
 });
+
+function showNotes() {
+    const notesList = document.querySelector('#notesList');
+
+    notesList.innerHTML = '';
+
+    notes.forEach(note => {
+        const oneNote = document.createElement('div');
+        oneNote.classList.add('oneNote');
+
+        //CREAR ELEMENTOS QUE CONFIGUREN UNA NUEVA NOTE
+        const label = document.createElement('label');
+        const input = document.createElement('input');
+        const span = document.createElement('span');
+        const content = document.createElement('div');
+        const actions = document.createElement('div');
+        const edit = document.createElement('button');
+        const deleteBtn = document.createElement('button');
+
+        input.type = 'checkbox';
+        input.checked = note.done;
+        span.classList.add('bubble');
+
+        //DETECTAR CATEGORÍA Y ASIGNAR CLASE
+        switch (note.slcCategory) {
+            case 'casa':
+                span.classList.add('casa');
+                break;
+            case 'trabajo':
+                span.classList.add('trabajo');
+                break;
+            case 'estudio':
+                span.classList.add('estudio');
+                break;
+    
+            default:
+                span.classList.add('casa');
+                break;
+        };
+
+        //ASIGNAR NOMBRE CLASES
+        content.classList.add('noteContent');
+        actions.classList.add('actions');
+        edit.classList.add('edit');
+        deleteBtn.classList.add('delete');
+
+        content.innerHTML = `<input type="text" value="${note.content}" readonly>`;
+        edit.innerHTML = 'Edit';
+        deleteBtn.innerHTML = 'Delete';
+
+        //PADRES E HIJOS
+        label.appendChild(input);
+        label.appendChild(span);
+        actions.appendChild(edit);
+        actions.appendChild(deleteBtn);
+        oneNote.appendChild(label);
+        oneNote.appendChild(content);
+        oneNote.appendChild(actions);
+
+        //FINALMENTE AGREGAMOS ONENOTE AL GRUPO notesList
+        notesList.appendChild(oneNote);
+
+        if (note.done) {
+            oneNote.classList.add('done');
+        }
+
+        input.addEventListener('click', (e) => {
+            note.done = e.target.checked;
+            localStorage.setItem('notes', JSON.stringify(notes));
+
+            if (note.done) {
+                oneNote.classList.add('done');
+            } else {
+                oneNote.classList.remove('done')
+            }
+
+            showNotes();
+        })
+
+        //FUNCION PARA BOTÓN DE EDITAR
+        edit.addEventListener('click', (e) => {
+            const input = content.querySelector('input');
+            input.removeAttribute('readonly');
+            input.focus();
+            input.addEventListener('blur', e => {
+                input.setAttribute('readonly', true);
+                note.content = e.target.value;
+                localStorage.setItem('notes', JSON.stringify(notes));
+
+                showNotes();
+            });
+        });
+
+        //FUNCION PARA BOTÓN DE BORRAR
+        deleteBtn.addEventListener('click', (e) => {
+            notes = notes.filter(t => t != note);
+            localStorage.setItem('notes', JSON.stringify(notes));
+
+            showNotes();
+        })
+    });    
+}
+
